e6d379898c9ea0fe1707710a9f09a176f39af26b
rajesh <rajeshkumaranraj@gmail.com>
2021-09-22 20:47:02 +0530

    Code cleanup

--- src/containers/App/reducer.js
+++ src/containers/App/reducer.js
@@ -1,14 +1,8 @@
-import { REGISTER_KEY_FOR_STOVE } from './constants';
-
 export const initialState = {};
 const reducer =
   (preloadedState = null) =>
   (state = preloadedState || initialState, action) => {
     switch (action.type) {
-      case REGISTER_KEY_FOR_STOVE: {
-        console.log('action.payload');
-        return state;
-      }
       default: {
         return { ...state };
       }
--- src/containers/App/saga.js
+++ src/containers/App/saga.js
@@ -1,17 +1,2 @@
-import { put, takeEvery } from 'redux-saga/effects';
-import { REGISTER_KEY_FOR_STOVE } from './constants';
-
-export function* registerStoveKeySaga(params = {}) {
-  try {
-    console.log('params in sga', params);
-  } catch (err) {
-    yield put(toggleErrorState(true));
-    console.error('Caught in homeSaga', err);
-  }
-}
-
-function* registerStoveKeyWatcher() {
-  yield takeEvery(REGISTER_KEY_FOR_STOVE, registerStoveKeySaga);
-}
-const appSagas = [registerStoveKeyWatcher()];
+const appSagas = [];
 export default appSagas;
