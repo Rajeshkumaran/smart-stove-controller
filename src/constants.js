44be38aaf7f35c05cbc3492612f822e7242e6179
rajesh <rajeshkumaranraj@gmail.com>
2021-11-12 13:45:50 +0530

    timer buzz sound updated

--- src/constants.js
+++ src/constants.js
@@ -4,5 +4,4 @@ export const primary_bg = '#F5F6FB';
 export const white = '#FFFFFF';
 export const slider_bg = '#CCD7E6';
 export const LIGHT_GREY = '#e9e9e2';
-export const TIMER_BUZZ_AUDIO_URL =
-  'https://firebasestorage.googleapis.com/v0/b/smart-stove-controller.appspot.com/o/timerBuzz.mp3?alt=media&token=a2acf534-31e2-4bb7-ba20-edb612b5719f';
+export const TIMER_BUZZ_AUDIO_URL = 'gs://smart-stove-controller.appspot.com/timerBuzzsound.mpeg';
